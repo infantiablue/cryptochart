7e84989cf16362d23001ea0b239823932fc0981f
Truong Phan <dangtruong@gmail.com>
2021-05-17 04:49:02 +0700

    update investment value

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -13,7 +13,7 @@ const Stats = ({ upTrend, chartData, sendErrMsg }) => {
 	const [velocity, setVelocity] = useState(0.0);
 	const [portfolioValue, setPortfolioValue] = useState(null);
 	const [loading, setLoading] = useState(true);
-	const initInvestment = 100000000;
+	const initInvestment = 120000000;
 	const fixedPortfolio = 36222007;
 	useEffect(async () => {
 		// Update stats when chartData changed
